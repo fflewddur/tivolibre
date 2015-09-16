6ee521122f058e8efb5a26313014ee0081ce304b
Todd Kulesza <todd@dropline.net>
2015-09-16 15:08:20 -0700

    Copy our JAR to a temp directory so we don't overwrite it during testing.

--- batchTest.py
+++ batchTest.py
@@ -20,7 +20,9 @@
 
 import argparse
 import os
+import shutil
 import subprocess
+import tempfile
 
 class bcolors:
     OKBLUE = '\033[94m'
@@ -29,80 +31,103 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-def main():
-    args = parseArgs()
-    printJarVersion(args)
-    testFilesInDir(args)
+class BatchTester:
+    args = None
+    jarPath = None
 
-def parseArgs():
-    parser = argparse.ArgumentParser()
-    defaultJarPath = os.path.join('.', 'jar', 'tivo-libre.jar')
-    parser.add_argument('-m', '--mak', help="MAK for files in the test directory", required=True)
-    parser.add_argument('-d', '--dir', help="Directory containing files to test", required=True,
-                        metavar='TEST_DIR', dest='testDir')
-    parser.add_argument('-r', help="Extension for reference files (default is .ref.mpg)",
-                        default='.ref.mpg', metavar='FILE_EXTENSION', dest='refExtension')
-    parser.add_argument('-t', help="Extension for TiVoLibre files (default is .mpg)",
-                        default='.mpg', metavar='FILE_EXTENSION', dest='ourExtension')
-    parser.add_argument('-s', help="Extension for source files (default is .TiVo)",
-                        default='.TiVo', metavar='FILE_EXTENSION', dest='sourceExtension')
-    parser.add_argument('--skip', help="Skip the first N TiVo files", metavar='N', type=int,
-                        default=0)
-    parser.add_argument('-j', '--jar',
-                        help="Path to the TivoLibre JAR (default is " + defaultJarPath +")",
-                        default=defaultJarPath, metavar="JAR_PATH")
-    return parser.parse_args()
+    def run(self):
+        self.args = self.parseArgs()
+        with tempfile.TemporaryDirectory() as tmpDir:
+            self.copyJarToTmpDir(tmpDir)
+            self.printJarVersion()
+            self.testFilesInDir()
 
-def printJarVersion(args):
-    result = subprocess.run(['java', '-jar', args.jar, '-v'], stdout=subprocess.PIPE)
-    print("Running with {}".format(result.stdout.decode('utf-8')), end='')
+    def parseArgs(self):
+        parser = argparse.ArgumentParser()
+        defaultJarPath = os.path.join('.', 'jar', 'tivo-libre.jar')
+        parser.add_argument('-m', '--mak', help="MAK for files in the test directory", required=True)
+        parser.add_argument('-d', '--dir', help="Directory containing files to test", required=True,
+                            metavar='TEST_DIR', dest='testDir')
+        parser.add_argument('-r', help="Extension for reference files (default is .ref.mpg)",
+                            default='.ref.mpg', metavar='FILE_EXTENSION', dest='refExtension')
+        parser.add_argument('-t', help="Extension for TiVoLibre files (default is .mpg)",
+                            default='.mpg', metavar='FILE_EXTENSION', dest='ourExtension')
+        parser.add_argument('-s', help="Extension for source files (default is .TiVo)",
+                            default='.TiVo', metavar='FILE_EXTENSION', dest='sourceExtension')
+        parser.add_argument('--skip', help="Skip the first N TiVo files", metavar='N', type=int,
+                            default=0)
+        parser.add_argument('-j', '--jar',
+                            help="Path to the TivoLibre JAR (default is " + defaultJarPath +")",
+                            default=defaultJarPath, metavar="JAR_PATH")
+        return parser.parse_args()
 
-def testFilesInDir(args):
-    filesWithDifferences = []
-    perfectFiles = []
-    numSkipped = 0
-    for path, subdirs, files in os.walk(args.testDir):
-        for name in files:
-            if isTivoFile(name, args):
-                if numSkipped >= args.skip:
-                    filePath = os.path.join(path, name)
-                    if not decodeFile(filePath, args):
-                        filesWithDifferences.append(filePath)
-                    else:
-                        perfectFiles.append(filePath)
+    def copyJarToTmpDir(self, tmpDir):
+        destPath = os.path.join(tmpDir, 'tivo-libre.jar')
+        shutil.copyfile(self.args.jar, destPath)
+        self.jarPath = destPath
+
+    def printJarVersion(self):
+        result = subprocess.run(['java', '-jar', self.jarPath, '-v'], stdout=subprocess.PIPE)
+        print("Running with {}".format(result.stdout.decode('utf-8')), end='')
+
+    def testFilesInDir(self):
+        tivoFiles = self.listSourceFiles()
+
+        numSkipped = 0
+        filesWithDifferences = []
+        perfectFiles = []
+        for filePath in tivoFiles:
+            if numSkipped >= self.args.skip:
+                if not self.decodeFile(filePath):
+                    filesWithDifferences.append(filePath)
                 else:
-                    numSkipped += 1
+                    perfectFiles.append(filePath)
+            else:
+                numSkipped += 1
 
-    if perfectFiles:
-        print("\nPerfectly decoded files:" + bcolors.OKGREEN)
-        for filePath in perfectFiles:
-            print("\t{}".format(filePath))
-        print(bcolors.ENDC)
+        self.printResults(perfectFiles, filesWithDifferences)
 
-    if filesWithDifferences:
-        print("Shit to fix:" + bcolors.FAIL)
-        for filePath in filesWithDifferences:
-            print("\t{}".format(filePath))
-        print(bcolors.ENDC)
+    def listSourceFiles(self):
+        sourceFiles = []
+        for path, subdirs, files in os.walk(self.args.testDir):
+            for name in files:
+                if self.isTivoFile(name):
+                    sourceFiles.append(os.path.join(path, name))
+        sourceFiles.sort()
+        return sourceFiles
 
-def isTivoFile(name, args):
-    return not name.startswith('.') and name.endswith(args.sourceExtension)
+    def isTivoFile(self, name):
+        return not name.startswith('.') and name.endswith(self.args.sourceExtension)
 
-def decodeFile(inputPath, args):
-    outputPath = inputPath.replace(args.sourceExtension, args.ourExtension)
-    print(bcolors.OKBLUE + "Testing {}{}".format(inputPath, bcolors.ENDC))
-    print("Decoding to {}...".format(outputPath))
-    subprocess.run(['java', '-jar', args.jar, '-m', args.mak, '-i', inputPath,
-                    '-o', outputPath])
-    referencePath = outputPath.replace(args.ourExtension, args.refExtension)
-    print("Comparing output to reference file {}...".format(referencePath))
-    result = subprocess.run(['diff', outputPath, referencePath])
-    if result.returncode is not 0:
-        print(bcolors.FAIL + "These files are different :(" + bcolors.ENDC)
-        return False
-    else:
-        print(bcolors.OKGREEN + "These files are identical!" + bcolors.ENDC)
-        return True
+    def decodeFile(self, inputPath):
+        outputPath = inputPath.replace(self.args.sourceExtension, self.args.ourExtension)
+        print(bcolors.OKBLUE + "Testing {}{}".format(inputPath, bcolors.ENDC))
+        print("Decoding to {}...".format(outputPath))
+        subprocess.run(['java', '-jar', self.jarPath, '-m', self.args.mak, '-i', inputPath,
+                        '-o', outputPath])
+        referencePath = outputPath.replace(self.args.ourExtension, self.args.refExtension)
+        print("Comparing output to reference file {}...".format(referencePath))
+        result = subprocess.run(['diff', outputPath, referencePath])
+        if result.returncode is not 0:
+            print(bcolors.FAIL + "These files are different :(" + bcolors.ENDC)
+            return False
+        else:
+            print(bcolors.OKGREEN + "These files are identical!" + bcolors.ENDC)
+            return True
+
+    def printResults(self, perfectFiles, filesWithDifferences):
+        if perfectFiles:
+            print("\nPerfectly decoded files:" + bcolors.OKGREEN)
+            for filePath in perfectFiles:
+                print("\t{}".format(filePath))
+            print(bcolors.ENDC)
+
+        if filesWithDifferences:
+            print("Shit to fix:" + bcolors.FAIL)
+            for filePath in filesWithDifferences:
+                print("\t{}".format(filePath))
+            print(bcolors.ENDC)
 
 if __name__ == "__main__":
-    main()
+    tester = BatchTester()
+    tester.run()
